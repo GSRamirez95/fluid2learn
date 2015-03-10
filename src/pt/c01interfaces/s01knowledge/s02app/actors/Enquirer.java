160bc9c62a8dcaadd0a61b29097f65138ede36ab
GSRamirez95 <g.s.ramirez1995@gmail.com>
2015-03-10 15:28:10 -0300

    trabalho 1

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -1,6 +1,10 @@
 package pt.c01interfaces.s01knowledge.s02app.actors;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import pt.c01interfaces.s01knowledge.s01base.impl.BaseConhecimento;
+import pt.c01interfaces.s01knowledge.s01base.impl.Pergunta;
 import pt.c01interfaces.s01knowledge.s01base.inter.IBaseConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IDeclaracao;
 import pt.c01interfaces.s01knowledge.s01base.inter.IEnquirer;
@@ -16,28 +20,75 @@ public class Enquirer implements IEnquirer
 	}
 	
 	
+	/**
+	 * Se conecta ao entrevistador fazendo perguntas referentes aos animais.
+	 * @param responder eh o entrevistador para perguntar
+	 */
+	
 	@Override
 	public void connect(IResponder responder)
-	{
-        IBaseConhecimento bc = new BaseConhecimento();
+	{	
+		boolean encontrado = false;
+        int animal = 0;
+        int questionCounter = 0;
+        ArrayList <Pergunta> perguntas = new ArrayList<Pergunta>();
+		IBaseConhecimento bc = new BaseConhecimento();
+		String listaAnimais[] = bc.listaNomes();
 		
-		obj = bc.recuperaObjeto("tiranossauro");
-
-		IDeclaracao decl = obj.primeira();
 		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
+        while (animal < listaAnimais.length && !encontrado) //Percorre todos animais do bd 
+        {
+        	obj = bc.recuperaObjeto(listaAnimais[animal]); //carrega info do animal
+	
+			IDeclaracao decl = obj.primeira();
+			Pergunta p;
+			boolean naoExiste;
 			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
+	        boolean animalEsperado = true;
+			while (decl != null && animalEsperado) {  //Percorre todas as infos do animal atual
+				String pergunta = decl.getPropriedade();
+				String resposta;
+				String respostaEsperada = decl.getValor();
+				
+				p = null;
+				naoExiste = true;
+				
+				Iterator<Pergunta>it = perguntas.iterator();
+				
+				while(it.hasNext()) {  //Percorre todas perguntas já feitas
+					p = it.next();
+		            if (p.getPergunta().equalsIgnoreCase(pergunta)) {
+		            	naoExiste = false;
+		            	p.incQuantidadeVezes();
+		            	break;
+		            }
+		        }
+				
+				if (naoExiste){  //Pergunta ao responder, se nao for repetir a pergunta
+					resposta = responder.ask(pergunta);
+					p = new Pergunta(pergunta, resposta);
+					perguntas.add(questionCounter++, p);
+					
+				}else{ 
+					resposta = p.getResposta();
+				}
+
+				//Se a resposta do enquirer for a esperada, pula para proxima do mesmo animal
+				// se nao, pula para o proximo animal.
+				if (resposta.equalsIgnoreCase(respostaEsperada))  
+					decl = obj.proxima();
+				else
+				{
+					animalEsperado = false;
+					animal++;
+					continue;
+				}
+			}
+			
+			encontrado = animalEsperado;
+        }
+        
+		boolean acertei = responder.finalAnswer(listaAnimais[animal]);
 		
 		if (acertei)
 			System.out.println("Oba! Acertei!");
